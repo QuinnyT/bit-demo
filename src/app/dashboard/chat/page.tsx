a7584a5c9e4101cfb4c67d6d266217c07316ee40
QuinnyT <472518620@qq.com>
2024-07-16 19:08:01 +0800

    add 'add chat box' button 2

--- src/app/dashboard/chat/page.tsx
+++ src/app/dashboard/chat/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 // import type { Metadata } from 'next';
 import Stack from '@mui/material/Stack';
 // import { config } from '@/config';
@@ -8,53 +8,18 @@ import Stack from '@mui/material/Stack';
 import { MySideNav } from '@/components/dashboard/layout/my-side-nav';
 import { ChatBot } from "@/components//dashboard/chatbot";
 import Button from '@mui/material/Button';
-import { ChatGPTMessage } from '@/lib/openai-stream';
 
 
 // export const metadata = { title: `Customers | Dashboard | ${config.site.name}` } satisfies Metadata;
-interface ApiResponse {
-  sender: string;
-  text: string;
-}
-
-interface Message {
-  role: string;
-  content: string;
-}
 
 export default function Page(): React.JSX.Element {
   const [chatLists, setChatLists] = useState(["chat 1"]);
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [messages, setMessages] = useState([]);
-  
-  useEffect(() => {
-    async function getHistoryData(): Promise<void> {
-      try {
-        const response = await fetch('/api/message');
-        if (!response.ok) {
-          throw new Error("获取聊天记录时出现问题");
-        }
-        const result = await response.json();
-        console.log("result", result)
-        const messages = result.map((res) => {
-          return {
-            role: res.sender,
-            content: res.text
-          }
-        })
-        setMessages(messages);
-      } catch (error) {
-        console.log("获取聊天记录时出现问题", error)
-      }
-    }
-    getHistoryData();
-  }, [])
   
   function addNewChat(): void {
     const newIndex = chatLists.length;
     setChatLists((prev) => [...prev, `chat${newIndex + 1}`]);
     setSelectedIndex(newIndex);
-    setMessages([])
   }
 
   function changeSelectedChat(chatIndex: number): void {
@@ -73,7 +38,7 @@ export default function Page(): React.JSX.Element {
             Add Chat Box
         </Button>
       </MySideNav>
-      <ChatBot messages={messages} setMessages={setMessages} />
+      <ChatBot />
     </Stack>
   );
 }
--- src/components/dashboard/chatbot/index.js
+++ src/components/dashboard/chatbot/index.js
@@ -12,30 +12,30 @@ import { useEffect, useState } from "react";
 import MessageList from "./components/message-list";
 import MessageInput from "./components/message-input";
 
-export function ChatBot({ messages, setMessages }) {
-  // const [messages, setMessages] = useState([]);
+export function ChatBot() {
+  const [messages, setMessages] = useState([]);
 
-  // useEffect(() => {
-  //   async function getHistoryData() {
-  //     try {
-  //       const response = await fetch('/api/message');
-  //       if (!response.ok) {
-  //         throw new Error("获取聊天记录时出现问题");
-  //       }
-  //       const result = await response.json();
-  //       const messages = result.map((res) => {
-  //         return {
-  //           role: res.sender,
-  //           content: res.text
-  //         }
-  //       })
-  //       setMessages(messages);
-  //     } catch (error) {
-  //       console.log("获取聊天记录时出现问题", error)
-  //     }
-  //   }
-  //   getHistoryData();
-  // }, [])
+  useEffect(() => {
+    async function getHistoryData() {
+      try {
+        const response = await fetch('/api/message');
+        if (!response.ok) {
+          throw new Error("获取聊天记录时出现问题");
+        }
+        const result = await response.json();
+        const messages = result.map((res) => {
+          return {
+            role: res.sender,
+            content: res.text
+          }
+        })
+        setMessages(messages);
+      } catch (error) {
+        console.log("获取聊天记录时出现问题", error)
+      }
+    }
+    getHistoryData();
+  }, [])
   
   async function handleNewMessage(value) {
     // console.log("value", value)
