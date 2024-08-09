37abeb70a258843d5cc382e8292b432b59ef6b65
QuinnyT <472518620@qq.com>
2024-08-09 16:33:18 +0800

    set demo messages

--- src/lib/demo-message-list.ts
+++ src/lib/demo-message-list.ts
@@ -1,10 +1,8 @@
 
 const demoMsgs = new Map();
 
-demoMsgs.set(1, "微重力实验需要准备以下物品：1.微重力实验装置 2.器官芯片1号 3.微重力流体实验箱 4.微重力物质传输系统 5.微重力颗粒分离器");
-demoMsgs.set(2, "小明拿取的物品包括：1....");
-demoMsgs.set(3, "xxx在xx舱段xx象限xxx");
-demoMsgs.set(4, "回复4");
-demoMsgs.set(5, "回复5");
+demoMsgs.set(1, "微重力实验需要准备以下物品：1、微重力实验装置； 2、器官芯片1号； 3、微重力流体实验箱； 4、微重力物质传输系统； 5、微重力颗粒分离器。");
+demoMsgs.set(2, "小明拿取的物品包括：1、螺丝刀； 2、扳手； 3、铲子； 4、锤子； 5、钳子。");
+demoMsgs.set(3, "螺丝刀在：舱段1-第一象限-货柜A");
 
 export { demoMsgs };
